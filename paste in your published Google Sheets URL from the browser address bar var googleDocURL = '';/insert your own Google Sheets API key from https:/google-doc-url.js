aec3ff38cf26814b6b8808e43b94f7d3b4aa2c26
sashadb <sashadbrown@gmail.com>
2021-08-13 13:36:14 -0400

    Create google-doc-url.js

---
+++ paste in your published Google Sheets URL from the browser address bar var googleDocURL = '';/insert your own Google Sheets API key from https:/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = '';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = '';
