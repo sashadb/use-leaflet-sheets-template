b11191bc3f145a47bbd4c36a0519f3ab7200416d
sashadb <sashadbrown@gmail.com>
2021-08-13 14:04:20 -0400

    redo

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yMICLr9u1agpPTzXgnuWk2FOo2406caX-Iweg-aF08w/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyD7f2Drkbza-ksOBy5Qq620efWBGCMdr7Y';
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
